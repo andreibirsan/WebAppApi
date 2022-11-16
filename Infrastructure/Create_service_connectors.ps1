54d8e41f7cd789790a2e9792425196dec4129efe
Andrei Birsan <andrei.birsan@cegeka.com>
2022-11-16 14:07:01 +0200

    /

--- Infrastructure/Create_service_connectors.ps1
+++ Infrastructure/Create_service_connectors.ps1
@@ -1,7 +1,3 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
 $WebAppName = Get-AzWebApp -ResourceGroupName tfrg_webapp-api
 $StorageAccount = Get-AzStorageAccount -ResourceGroupName tfrg_webapp-api
 $CosmosDbAccount = Get-AzCosmosDBAccount -ResourceGroupName tfrg_webapp-api
